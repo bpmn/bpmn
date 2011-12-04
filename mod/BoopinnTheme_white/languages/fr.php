a9dbde510876c4fbd5a5d1f51b5f8957a325b3cd
Philippe Bressy <pbressy@gmail.com>
2011-12-04 14:46:39 +0000

    pour du francais

---
+++ mod/BoopinnTheme_white/languages/fr.php
@@ -0,0 +1,33 @@
+<?php
+
+	$french = array(
+			'register:error:nameshort' => "Votre identifiant sur le site est trop court",
+			'register:error:usershort' => "Le nom d'utilisateur est trop court",
+			'register:error:emailshort' => "Votre email n'est pas valide",
+			'register:error:passwordshort' => "Le mot de passe est trop court",
+			'register:error:passwordmatch' => "Les mots de passe ne corresponde pas",
+			'register:error:emailtaken' => "Il y a deja un compte enregistre pour cet addresse email (essayez de vous identifier)",
+			'user:email:notfound' => "Email %s non trouvee.",
+			'username/email' => "Utilisateur/Email",
+			'user:usernameemail' => "Utilisateur ou Email",
+			
+			'email:resetreq:body:wusername' => "Bonjour %s,
+			
+Une personne (de l'addresse IP %s) a fait une requete de nouveau mot de passe pour leur compte Boopinn.
+Si vous avez fait cette demande, veuillez cliquer sur le lien ci dessous, sinon, ignorez cet email.
+
+Pour rappel, votre nom d'utilisateur est: %s
+
+%s",
+	'agreetoterms' => "J'ai lu et accepte les",
+	'terms' => "Termes et Conditions d'utilisations",
+
+	'agreetoterms:required' => "Vous devez lire et accepter les Termes et Conditions d'utlisations",
+
+        'toolbar:logout' => "Sortir",
+
+
+	);
+					
+	add_translation("fr",$french);
+?>
