fa235fac08759f7c4a35a40760796ccbb49401d5
Philippe Bressy <pbressy@gmail.com>
2011-12-04 14:46:24 +0000

    pour du francais

---
+++ mod/boopinn_dashboard/languages/fr.php
@@ -0,0 +1,24 @@
+<?php
+/**
+	 * Elgg groups plugin language pack
+	 *
+	 * @package ElggGroups
+	 */
+
+	$french = array(
+                        'dashboard:createcis' => "Ajoutez votre contribution au Reseau Boopinn and votre CIS!",
+                        'dashboard:createopenlab:text' =>"Pour collaborer sur une idee a developer, probleme a resoudre, et arriver ad des solutions creatives et innovantes",
+                        'dashboard:createopenlab:button' =>"Creer un OpenLab",
+                        'dashboard:createteam:text'=>"Developez l'exposition de votre travail au travers du reseau Boopinn, experts et enthousiastes vers votre equipe",
+                        'dashboard:createteam:title' =>"Presentez votre compagnie, votre equipe ou projet!",
+                        'dashboard:createteam:button' =>"Creer une Team",
+            		'dashboard:noactivity' => "Pas d'activites",
+			'dashboard:title:activity' => "Mes %s activites",
+                        'dashboard:title:cisactivity'=>"Nouvelles equipes (Teams) et OpenLab dans mon Cis",
+                        'dashboard:title:followactivity'=>"Activite suivies" , 
+			'dashboard:topthreeuserss' => 'Top 3 utilisateurs ',
+                        'dashboard:welcome' => 'Bienvenue' 
+		       	
+        );
+    add_translation("fr",$french);
+?>
